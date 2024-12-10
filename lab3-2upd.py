543e301ccc0e8a0f73948a5730ffd6372e07faed
nikittr <111736605+nikittr@users.noreply.github.com>
2024-12-10 22:23:40 +0300

    Add files via upload

---
+++ lab3-2upd.py
@@ -0,0 +1,11 @@
+def find_common_participants(first_group, second_group, split_sym=','):  # Напишите функцию find_common_participants
+    first_set = set(first_group.split(split_sym))  # списки создаем, разделяя по символу строку
+    second_set = set(second_group.split(split_sym))  # затем получаем множества из списков для последующего пересечения
+    intesec_list = list(first_set.intersection(second_set))  # список пересения двух множеств
+    return intesec_list.sort()  # сортируем в алфавитном порядке итоговый список
+
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+print(find_common_participants(participants_first_group, participants_second_group, '|'))  #
